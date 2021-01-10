e60c4facfbaf6b2b04d6c65bb925efd456fdc17d
MarlenePiekny <marlene.piekny@gmail.com>
2021-01-10 19:02:21 +0100

    Les 3 pages .html deviennent index.php, hobby.php et contact.php

---
+++ hobby.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+
+  <main class="hobby">
+    <h1>Peinture aquarelle</h1>
+    <h2>En bref</h2>
+    <p>L'aquarelle est une peinture à l'eau sur papier. Les pigments dilués dans l'eau laissent apparaître le support
+      papier qui apporte déjà un fond. Sa simplicité n'est qu'apparente et demande de la technique et surtout du
+      lâcher-prise.</p>
+    <h2>Vidéo tutoriel</h2>
+    <iframe width="517" height="291" src="https://www.youtube.com/embed/f3mPWOZ1Xug" frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen></iframe>
+  </main>
+
---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+
+  <main>
+    <h1>A propos</h1>
+    <p>De nature curieuse, appliquée et avec un goût pour les applications digitales, je suis une
+      formation de 6 mois en <strong>développement web</strong>. J'ai envie d'être au coeur des solutions qu'apportent
+      le numérique
+      notamment celles autour de l'expérience utilisateur. Je souhaite par la suite mettre mes nouvelles compétences au
+      service d'une entreprise de Valence ou des environs pour mon <strong>alternance</strong> de 12 mois.
+    </p>
+
+    <h2>Mes compétences</h2>
+    <ul>
+      <li>Autonome</li>
+      <li>Rigoureuse</li>
+    </ul>
+
+    <h2>Mes expériences</h2>
+    <table class="tableau">
+
+      <thead>
+        <tr>
+          <th>Durée</th>
+          <th>Entreprise</th>
+          <th>Lieu</th>
+          <th>Poste</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>septembre 2020</td>
+          <td>Domaine Combier</td>
+          <td>Pont de l'Isère (26)</td>
+          <td>Vendanges et activités en cave</td>
+        </tr>
+
+        <tr>
+          <td>novembre 2018 - août 2020</td>
+          <td>Thales DMS</td>
+          <td>Mérignac (33)</td>
+          <td>Chargée d'innovation et transformation digitale - Alternance</td>
+        </tr>
+
+        <tr>
+          <td>novembre 2015 - août 2018</td>
+          <td>ESSEC Programme Bachelor</td>
+          <td>Cergy-Pontoise (95)</td>
+          <td>Assistante pédagogique - Alternance</td>
+        </tr>
+
+        <tr>
+          <td>février 2015 - août 2015</td>
+          <td>Phone Régie pour EY</td>
+          <td>La Défense (92)</td>
+          <td>Hôtesse d'accueil</td>
+        </tr>
+      </tbody>
+
+    </table>
+
+
+  </main>
+
