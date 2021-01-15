f717eacf6cf77504dd7faedb01f3d7c8fc7f95fe
MarlenePiekny <marlene.piekny@gmail.com>
2021-01-15 13:07:42 +0100

    Le fichier contact receuille les champs du formulaire à la ligne, des messages d'erreur s'affichent en fonction du contenu des champs

--- pages/contact.php
+++ pages/contact.php
@@ -11,7 +11,7 @@ if (filter_has_var(INPUT_POST, 'soumis')) {
     $civilitecontact = filter_input(INPUT_POST, 'civilite_contact');
     $nomcontact = filter_input(INPUT_POST, 'nom_contact');
     $prenomcontact = filter_input(INPUT_POST, 'prenom_contact');
-    $emailcontact = filter_input(INPUT_POST, 'email_contact');
+    $emailcontact = filter_input(INPUT_POST, 'email_contact', FILTER_VALIDATE_EMAIL);
     $raisoncontact = filter_input(INPUT_POST, 'raison_contact');
     $messagecontact = filter_input(INPUT_POST, 'message_contact');
 
@@ -21,27 +21,30 @@ if (filter_has_var(INPUT_POST, 'soumis')) {
                     !empty($prenomcontact)) &&
                 !empty($emailcontact)) &&
             !empty($raisoncontact)) &&
-        !empty($messagecontact))) {
+        (!empty($messagecontact) && (strlen($messagecontact) > 5)))) {
 
         //Le formulaire est complet : on traite le formulaire les champs pour les envoyer dans un fichier horodaté contact_Y-m-d-H-i-s.txt horodaté
 
         //Affectation de l'horadatage Y-m-d-H-i-s" au nom du fichier
-        $fichiercontact = 'contact/contact_' . date("Y-m-d-H-i-s") . '.txt';
+        date_default_timezone_set('Europe/Paris');
+        $fichiercontact = 'contact/contact_' . date('Y-m-d-H-i-s') . '.txt';
 
-        //Création du fichier horodaté contact"Y-m-d-H-i-s" dans le repertoire contact
-        file_put_contents($fichiercontact, $civilitecontact, FILE_APPEND);
+        //Création du fichier horodaté contact"Y-m-d-H-i-s" dans le repertoire contact qui prend les valeurs des différents champs
+        $donneescontact = $civilitecontact . "\n" . $nomcontact . "\n" . $prenomcontact . "\n" . $emailcontact . "\n" . $raisoncontact . "\n" . $messagecontact;
+        file_put_contents($fichiercontact, $donneescontact);
+        /*file_put_contents($fichiercontact, $civilitecontact, FILE_APPEND);
         file_put_contents($fichiercontact, $nomcontact, FILE_APPEND);
         file_put_contents($fichiercontact, $prenomcontact, FILE_APPEND);
         file_put_contents($fichiercontact, $emailcontact, FILE_APPEND);
         file_put_contents($fichiercontact, $raisoncontact, FILE_APPEND);
-        file_put_contents($fichiercontact, $messagecontact, FILE_APPEND);
+        file_put_contents($fichiercontact, $messagecontact, FILE_APPEND);*/
 
         //Afficher un message quand les infos du formulaire ont été envoyées
         echo("Je vous remercie, votre formulaire a bien été envoyé");
 
     } else {
         //Le formulaire n'est pas complet
-        echo("Veuillez saisir tous les champs du formulaire");
+        echo ("Veuillez saisir tous les champs du formulaire");
     }
 }
 //Le formulaire n'est pas envoyé
@@ -53,7 +56,7 @@ if (filter_has_var(INPUT_POST, 'soumis')) {
     <form action="index.php?page=contact" method="post">
 
         <div style="color:red">
-        <?php /*Vérification des champs et message d'erreur si vide et que le formulaire est envoyé, rien si OK*/
+        <?php //Vérification des champs et message d'erreur
         if (empty($civilitecontact) && filter_has_var(INPUT_POST, 'soumis')){
             echo ("La civilité n'est pas complétée ");
         }
@@ -68,7 +71,7 @@ if (filter_has_var(INPUT_POST, 'soumis')) {
         </div>
 
         <div style="color:red">
-        <?php /*Vérification des champs et message d'erreur si vide, rien si OK*/
+        <?php // Vérification des champs et message d'erreur
         if (empty($nomcontact) && filter_has_var(INPUT_POST, 'soumis')){
         echo ("Le nom n'est pas complété");
         }
@@ -80,7 +83,7 @@ if (filter_has_var(INPUT_POST, 'soumis')) {
         </div>
 
         <div style="color:red">
-        <?php /*Vérification des champs et message d'erreur si vide, rien si OK*/
+        <?php //Vérification des champs et message d'erreur
         if (empty($prenomcontact) && filter_has_var(INPUT_POST, 'soumis')){
         echo ("Le prénom n'est pas complété");
         }
@@ -92,19 +95,23 @@ if (filter_has_var(INPUT_POST, 'soumis')) {
         </div>
 
         <div style="color:red">
-        <?php /*Vérification des champs et message d'erreur si vide, rien si OK*/
-        if (empty($emailcontact) && filter_has_var(INPUT_POST, 'soumis')){
-        echo ("L'e-mail' n'est pas complété");
+        <?php //Vérification des champs et message d'erreur
+        if ((empty(filter_input(INPUT_POST, 'email_contact'))) && filter_has_var(INPUT_POST, 'soumis')){
+            //Si le champs n'est pas complété et que le formulaire est soumis
+            echo ("L'e-mail n'est pas complété");
+        } elseif ((empty(filter_input(INPUT_POST, 'email_contact', FILTER_VALIDATE_EMAIL))) && filter_has_var(INPUT_POST, 'soumis')){
+            //Sinon si le formulaire est soumis et que le champs n'est pas au bon format
+            echo ("Veuillez saisir une adresse email au format : xxxxx@yyyy.zz");
         }
         ?>
         </div>
         <div class="form-champ">
             <label for="email">e-mail : </label>
-            <input type=”email" id="email" name="email_contact">
+            <input type=”text" id="email" name="email_contact">
         </div>
 
         <div style="color:red">
-        <?php/*Vérification des champs et message d'erreur si vide, rien si OK*/
+        <?php //Vérification des champs et message d'erreur
         if (empty($raisoncontact) && filter_has_var(INPUT_POST, 'soumis')){
         echo ("La raison n'est pas complétée");
         }
@@ -119,9 +126,13 @@ if (filter_has_var(INPUT_POST, 'soumis')) {
         </div>
 
         <div style="color:red">
-        <?php /*Vérification des champs et message d'erreur si vide, rien si OK*/
+        <?php // Vérification des champs et message d'erreur
         if (empty($messagecontact) && filter_has_var(INPUT_POST, 'soumis')){
-        echo ("Le message n'est pas complété");
+            //Si le formulaire est soumis et que le champs n'est pas complété
+            echo ("Le message n'est pas complété");
+        } elseif  ((strlen($messagecontact) < 5) && filter_has_var(INPUT_POST, 'soumis')) {
+            //Sinon si le formulaire est soumis et que le champs n'a pas plus de 5 caractères
+            echo ("Veuillez envoyer un message de plus de 5 caractères s'il vous plait");
         }
         ?>
         </div>
